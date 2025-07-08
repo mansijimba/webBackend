a8898728d0aba49357767b436a3c7adda177d55d
Mansi <mansezimba14@gmail.com>
2025-07-08 10:57:59 +0545

    update

--- middlewares/fileupload.js
+++ middlewares/fileupload.js
@@ -1,33 +1,31 @@
-const multer = require ("multer")
-const{v4: uuidv4} = require ("uuid")
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
-    destination:(req, file, cb) => cb(null,"uploads/"),
-    filename: (req, file, cb) => {
-        const ext = file.originalname.split(".").pop()
-        const filename = `${file.fieldname}- ${uuidv4()}.${ext}`
-        cb(null,filename)
-    }
-})
-const filefilter = (req, file, cb) =>{
-    if(file.minetype.startsWith("image")) cb(null, true)
-        else cb(new Error("only image allowed"),false)
-}
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const ext = file.originalname.split(".").pop();
+    const filename = `${file.fieldname}-${uuidv4()}.${ext}`;
+    cb(null, filename);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
 const upload = multer({
-    storage,
-    limits: {fileSize: 5* 1024* 1024},
-    filefilter
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter,
+});
 
-})
-
-module.exports ={
-    single: (fieldName) =>
-        upload.single(fieldName),
-    array: (fieldName, maxCount) =>
-        upload.array(
-            fieldName, maxCount
-        ),
-        fields: (fieldsArray) =>
-            upload.fields(fieldsArray)
-
-}
+module.exports = {
+  single: (fieldName) => upload.single(fieldName),
+  array: (fieldName, maxCount) => upload.array(fieldName, maxCount),
+  fields: (fieldsArray) => upload.fields(fieldsArray),
+};
