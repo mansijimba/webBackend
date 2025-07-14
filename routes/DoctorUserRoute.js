138a3c3a6dc49bef94d6862a87717a331c9fe495
Mansi <mansezimba14@gmail.com>
2025-07-14 23:56:42 +0545

    testing2

--- routes/DoctorUserRoute.js
+++ routes/DoctorUserRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const doctorUserController= require('../controllers/DoctorUserController');
+const doctorUserController= require('../controllers/user/DoctorUserController');
 
 router.get('/', doctorUserController.getPublicDoctors );
+router.get('/:id',doctorUserController.getDoctorById);
 
 module.exports = router;
